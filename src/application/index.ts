86e37957479eef9080ffcd55aca210508040b5af
Daniel Buriticá <dani.ferbu@gmail.com>
2025-08-19 16:30:06 -0500

    Merge pull request #1 from danburitica/feat/init-config
    feat: Adds initial exports for the application, domain, infrastructure, and shared layers, organizing the project structure

---
+++ src/application/index.ts
@@ -0,0 +1,3 @@
+// Application layer exports
+export * from './use-cases';
+export * from './dto';
---
+++ src/infrastructure/index.ts
@@ -0,0 +1,5 @@
+// Infrastructure layer exports
+export * from './config';
+export * from './persistence';
+export * from './controllers';
+export * from './mappers';
